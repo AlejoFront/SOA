37a12ef638ff895ac81e524ae4b559279bdd30fb
Alejandro <artificialbyte.sas@gmail.com>
2020-08-25 15:10:15 -0500

    calculadora con varias operaciones

--- ProyectoServidorCalculadoraSW/src/servicios/ServicioCalculadora.java
+++ ProyectoServidorCalculadoraSW/src/servicios/ServicioCalculadora.java
@@ -8,4 +8,21 @@ public class ServicioCalculadora {
     public static double sumar(double x, double y){
         return x+y;
     }
+    
+    public static double restar(double x, double y){
+        return x-y;
+    }
+    
+    public static double multiplicar(double x, double y){
+        return x*y;
+    }
+    
+    public static double dividir(double x, double y){
+        return x/y;
+    }
+    
+    public static double raiz(double x){
+        double sqrt = Math.abs(x);
+        return sqrt;
+    }
 }
--- ProyectoServidorCalculadoraSW/src/serviciosWeb/CalculadoraSW.java
+++ ProyectoServidorCalculadoraSW/src/serviciosWeb/CalculadoraSW.java
@@ -13,7 +13,28 @@ public class CalculadoraSW {
     }
 
     @WebMethod
-    public double sumar(@WebParam(name = "arg0") double x, @WebParam(name = "arg1") double y){
+    public double suma(@WebParam(name = "arg0") double x, @WebParam(name = "arg1") double y){
         return ServicioCalculadora.sumar(x, y);
     }
+
+    @WebMethod
+    public double resta(@WebParam(name = "arg0") double x, @WebParam(name = "arg1") double y){
+        return ServicioCalculadora.restar(x, y);
+    }
+
+    @WebMethod
+    public double multiplicacion(@WebParam(name = "arg0") double x, @WebParam(name = "arg1") double y){
+        return ServicioCalculadora.multiplicar(x, y);
+    }
+
+    @WebMethod
+    public double division(@WebParam(name = "arg0") double x, @WebParam(name = "arg1") double y){
+        return ServicioCalculadora.dividir(x, y);
+    }
+    
+    @WebMethod
+    public double raiz(@WebParam(name = "arg0") double x){
+        return ServicioCalculadora.raiz(x);
+    }
+    
 }
